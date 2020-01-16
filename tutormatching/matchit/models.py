37eb8583beacca72e68182a045669cb5407514c9
Kevin <kevin.tarjono@gmail.com>
2020-01-16 23:36:50 +0800

    filling the database and change RequestTutor model

--- tutormatching/matchit/models.py
+++ tutormatching/matchit/models.py
@@ -4,15 +4,16 @@ import json
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=200, default="0")
-    image_url = models.CharField(max_length=200, default="https://media.publit.io/file/matchit/p1-d.jpeg")
+    name = models.CharField(max_length=200)
+    
     
 class RequestTutor(models.Model):
     person = models.ForeignKey(Profile, on_delete=models.CASCADE)
     course_code = models.CharField(max_length=200)
     role = models.CharField(max_length=200)                             # teacher or student
-    email = models.CharField(max_length=200, default="0")
-    contact_no = models.CharField(max_length=200, default="0")
+    email = models.CharField(max_length=200, default="johndoe@gmail.com")
+    image_url = models.CharField(max_length=200, default="https://media.publit.io/file/matchit/p1-d.jpeg")
+    contact_no = models.CharField(max_length=200, default="01234567")
     satisfied = models.CharField(max_length=200, default = "No")
 
     def toJSON(self):
--- tutormatching/matchit/views.py
+++ tutormatching/matchit/views.py
@@ -58,10 +58,16 @@ def register(request):
         return render(request, 'register.html')
 
 def listRequest(request):
+    # return in JSON
+
     results = list(RequestTutor.objects.filter(satisfied = "No"))
     results_json = serializers.serialize('json', results)
     print(results)
     return HttpResponse(results_json)
+
+    # to render without react
+    # results = RequestTutor.objects.filter(satisfied = "No")
+    # return render(request, 'request.html', {'results':results})       
 
 def logout(request):
     auth.logout(request)
